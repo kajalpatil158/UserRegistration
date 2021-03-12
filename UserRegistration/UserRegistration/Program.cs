8b322afbb00b097f681920efc6177b7f7c7e1189
Kajal <kajalpatil158@gmail.com>
2021-03-12 11:37:19 +0530

    [Kajal Patil]Added: User_Regestration_Form_Start_With_First_Name

---
+++ UserRegistration/UserRegistration/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegistrationForms registration = new RegistrationForms();
+            Console.WriteLine("Enter First Name");
+            string Firstname = Console.ReadLine();
+            Console.WriteLine("First Name is= {0}", registration.ValidFirstName("Kajal"));
+        }
+    }
+}
---
+++ UserRegistration/UserRegistration/RegistrationForm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+
+namespace UserRegistration
+{
+    public class RegistrationForms
+    {
+        string First_Name = "^[A-Za-z]";
+        public bool ValidFirstName(string firstname) => Regex.IsMatch(firstname, First_Name);
+
+    }
+}
