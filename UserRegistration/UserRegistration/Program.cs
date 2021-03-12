d434dc060eff684cb306b19072bb45eec2640677
Kajal <kajalpatil158@gmail.com>
2021-03-12 16:48:44 +0530

    [Kajal PAtil]Added: Mobile Number

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -9,15 +9,20 @@ namespace UserRegistration
             RegistrationForms registration = new RegistrationForms();
             Console.WriteLine("Enter First Name");
             string Firstname = Console.ReadLine();
-            Console.WriteLine("First Name is= {0}", registration.ValidFirstName);
+            Console.WriteLine("First Name is= {0}", registration.ValidFirstName("kajal"));
 
             Console.WriteLine("Enter Last Name");
             string Lastname = Console.ReadLine();
-            Console.WriteLine("Last Name is= {0}", registration.ValidLastName("Patil"));
+            Console.WriteLine("Last Name is= {0}", registration.ValidLastName("patil"));
 
             Console.WriteLine("Enter Email_Id");
             string email = Console.ReadLine();
-            Console.WriteLine("Email_ID is= {0}", registration.ValidEmail("Kajal158gmail.com"));
+            Console.WriteLine("Email_ID is= {0}", registration.ValidEmail("kajal158@gmail.com"));
+
+            Console.WriteLine("Enter Mobile Number");
+            string mobile_number = Console.ReadLine();
+            Console.WriteLine("Mobile Number is= {0}", registration.ValidEmail("8600208238"));
+
         }
     }
 }
--- UserRegistration/UserRegistration/RegistrationForm.cs
+++ UserRegistration/UserRegistration/RegistrationForm.cs
@@ -11,9 +11,11 @@ namespace UserRegistration
         string First_Name = "^[A-Za-z]";
         string Last_Name = "^[A-Za-z]";
         string Email_Id = "^[a-z0-9A-Z]+([.#_+-][a-z0-9A-Z]+)*[@][a-z0-9A-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2})*$";
+        string Mobile_Number = "^[91]{2}[ ][0-9]{10,10}$";
         public bool ValidFirstName(string firstname) => Regex.IsMatch(firstname, First_Name);
         public bool ValidLastName(string lastname) => Regex.IsMatch( lastname, Last_Name);
         public bool ValidEmail(string email) => Regex.IsMatch(email, Email_Id);
+        public bool ValidMobileNumber(string mobile_number) => Regex.IsMatch(mobile_number, Mobile_Number);
 
     }
 }
