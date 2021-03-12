663f41963f144571a97effcc06cfeaff6cabb9c0
Kajal <kajalpatil158@gmail.com>
2021-03-12 16:40:47 +0530

    [Kajal Patil]Added: Email_Id

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -9,11 +9,15 @@ namespace UserRegistration
             RegistrationForms registration = new RegistrationForms();
             Console.WriteLine("Enter First Name");
             string Firstname = Console.ReadLine();
-            Console.WriteLine("First Name is= {0}", registration.ValidFirstName("Kajal"));
+            Console.WriteLine("First Name is= {0}", registration.ValidFirstName);
 
             Console.WriteLine("Enter Last Name");
             string Lastname = Console.ReadLine();
-            Console.WriteLine("Last Name is= {0}", registration.ValidFirstName("Kajal"));
+            Console.WriteLine("Last Name is= {0}", registration.ValidLastName("Patil"));
+
+            Console.WriteLine("Enter Email_Id");
+            string email = Console.ReadLine();
+            Console.WriteLine("Email_ID is= {0}", registration.ValidEmail("Kajal158gmail.com"));
         }
     }
 }
--- UserRegistration/UserRegistration/RegistrationForm.cs
+++ UserRegistration/UserRegistration/RegistrationForm.cs
@@ -10,7 +10,10 @@ namespace UserRegistration
     {
         string First_Name = "^[A-Za-z]";
         string Last_Name = "^[A-Za-z]";
+        string Email_Id = "^[a-z0-9A-Z]+([.#_+-][a-z0-9A-Z]+)*[@][a-z0-9A-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2})*$";
         public bool ValidFirstName(string firstname) => Regex.IsMatch(firstname, First_Name);
+        public bool ValidLastName(string lastname) => Regex.IsMatch( lastname, Last_Name);
+        public bool ValidEmail(string email) => Regex.IsMatch(email, Email_Id);
 
     }
 }
